287d0843fdb8b6f95a192670af7476886f412755
tcomr <tcomraaee@gmail.com>
2021-03-11 17:18:48 -0500

    updated

--- ts_c4.R
+++ ts_c4.R
@@ -32,39 +32,6 @@ autoplot(beer2) +
 # 11 values for each of the methods 
 #=======================HW March 11=============================
 
-# Naive  
-naive_algo = naive(beer2, h = 11) 
-summary(naive_algo)
-Naive_ = summary(naive_algo)[,1 ]
-
-# Mean  
-mean_algo = naive(beer2, h = 11) 
-summary(mean_algo)
-Mean_ = summary(mean_algo)[,1 ]
-Mean_
-
-# Seasonal Naive
-snaive_algo = snaive(beer2, h = 11)
-summary(snaive_algo)
-Seasonal_naive = summary(snaive_algo)[,1 ]
-
-
-# Drift  
-drift_algo = rwf(beer2, drift = TRUE, h = 11) 
-summary(drift_algo)
-drift_ = summary(drift_algo)[,1 ]
-drift_
-
-row_names = rownames(summary(naive_algo))
-row_names
-
-forecasts = matrix(c(Mean_, Naive_, Seasonal_naive, drift_), nrow = 11)
-forecasts
-# Convert to data frame with row and columns 
-forcased_values = data.frame(forecasts, row.names = row_names)
-colnames(forcased_values) = c("Mean", "Naive", "Seaonal Naive", "Drift")
-forcased_values
-
 
 
 
