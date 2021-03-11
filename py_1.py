778a57271e9c1608d7fe67519018a5a089e01b09
tcomr <tcomraaee@gmail.com>
2021-03-11 18:16:16 -0500

    heads &legs to find chicken and rabit

--- py_1.py
+++ py_1.py
@@ -1,3 +1,4 @@
+# Ctrl +Shift + C for multiline comment 
 
 print("hello")
 
@@ -10,3 +11,32 @@ def my_func(x):
   return(res)
   
 my_func(6)
+
+# source: https://github.com/zhiwehu/Python-programming-exercises/commit/5dc0cfeb09d2ecd0909f499a345be83b28c81590
+
+  # 35 head and 94 legs how many chicken and rabits
+  
+def rabit_chicken(heads, legs):
+  count = 0
+  count = legs - 2 * heads
+  count = count / 2
+  return count, (heads - count)
+rabit, chicken = rabit_chicken(35, 90)
+print("rabit = {}, Chicken = {}".format(rabit, chicken))
+  
+  
+  # res = 'Not possible '
+  # for i in range(heads+1):
+  #   j = heads - i
+  #   if 2*i + 4*j == legs:
+  #     return i, j 
+  #   return res, res
+  #   
+  #   
+    
+    
+    
+  
+  
+  
+  
