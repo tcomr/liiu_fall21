f26b0e91dde98e6fdef0ada807a4bcda80fb58a8
tcomr <tcomraaee@gmail.com>
2021-03-13 00:27:20 -0500

    for loop next break

--- intermediate R.R
+++ intermediate R.R
@@ -156,3 +156,118 @@ while (i <= 10) {
   }
   i <- i + 1
 }
+
+
+############====For loop ============================================================
+
+nums <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Loop version 1
+for ( l in nums){
+  print(l)
+}
+
+
+
+# Loop version 2
+for (i in 1:length(nums)){
+  print(nums[i])
+}
+
+
+# Loop over a list 
+
+nyc <- list(pop = 8405837, 
+            boroughs = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"), 
+            capital = FALSE)
+?list
+
+# Loop version 1
+for (bor in nyc){
+  print(bor)
+}
+
+# Loop version 2
+for (i in 1: length(nyc)){
+  print(nyc[[i]])
+}
+
+# loop over a matrix 
+m = c('x', 'na','0', '0', 'x', 'na', 'o', 'na', 'x', 'x', 'na', '0')
+t = matrix(m, nrow = 3)
+t
+
+# define the double for loop
+for (i in 1:nrow(t)) {
+  for (j in 1:ncol(t)) {
+    print(paste("On row ",i," and column ", j, " the board contains ", t[i, j]))
+  }
+}
+
+ncol(t) # 4
+nrow(t) # 3
+
+# mixed for and if else
+
+my_num <- c(16, 9, 13, 5, 2, 17, 14)
+
+# for loop with conditionals
+for (i in my_num) {
+  if (i > 9) {
+    print(i)
+    print("Large number with double digit!")
+    
+  } else {
+    print("single digit!")
+    
+  }
+  print(i)
+}
+
+# break: statement abandons the activeloop 
+# next: skips the remainder of the code in the loop, but continues to the next iteration 
+
+
+# The linkedin vector has already been defined for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+
+# Adapt/extend the for loop
+for (li in linkedin) {
+  if (li > 10) {
+    print("You're popular!")
+  } else {
+    print("Be more visible!")
+  }
+  
+  if (li > 16) {
+    print("This is ridiculous, I'm outta here!")
+    break
+  }
+  
+  if (li < 5) {
+    print("This is too embarrassing!")
+    next 
+  }
+  
+  print(li)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
