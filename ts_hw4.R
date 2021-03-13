ef3bab324bcd7b056fb9abb0a3a8277a88da1918
tcomr <tcomraaee@gmail.com>
2021-03-12 22:30:54 -0500

    nt

--- ts_hw4.R
+++ ts_hw4.R
@@ -38,7 +38,7 @@ row_names = rownames(summary(naive_algo))
 
 forecasts = matrix(c(Mean_, Naive_, Seasonal_naive, drift_), nrow = 11)
 forecasts
-# Convert to data frame with row and columns 
+# Convert to data frame with rows and columns 
 forcased_values = data.frame(forecasts, row.names = row_names)
 colnames(forcased_values) = c("Mean", "Naive", "Seaonal Naive", "Drift")
 forcased_values
