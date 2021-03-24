86829b87e7dbcdaa44847ab3d075f15b677493fa
tcomr <tcomraaee@gmail.com>
2021-03-23 21:51:49 -0400

    business data anaysis class note

---
+++ ba5_323.R
@@ -0,0 +1,16 @@
+
+# March 23
+# Today:
+  # Book: Business Data Science by Matt Taddy
+    # chapter 8: text as Data 
+
+# what is Big data? 
+  # number of observation < number fo features
+  # fat data: small rows(100 obs.) and a lot variables (1million feature)
+
+# whenever you cannot save data in computer memory (RAM), 
+  # that is big data - computer engineering 
+
+# length of dataset - traditional def 
+# rate at whihch we recieve data (eg. netflix) is big data - traditioanl def
+
