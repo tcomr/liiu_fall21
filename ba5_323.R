1d4af5361eb517130213eae7166db377a507d569
tcomr <tcomraaee@gmail.com>
2021-03-23 23:02:23 -0400

    marginal regression, betas or coeff of regression

--- ba5_323.R
+++ ba5_323.R
@@ -12,5 +12,56 @@
   # that is big data - computer engineering 
 
 # length of dataset - traditional def 
-# rate at whihch we recieve data (eg. netflix) is big data - traditioanl def
+# rate at which we recieve data (eg. netflix) is big data - traditioanl def
+
+
+
+# coding =======================================
+
+# data gasoline.csv from bds 
+rm(list = ls())
+getwd()
+ # data source:
+  # https://github.com/TaddyLab/BDS/blob/master/examples/gasoline.csv
+gas = read.csv("C:/Users/sts/Downloads/gasoline.csv")
+str(gas) # 'data.frame':	60 obs. of  402 variables
+
+# dependent variable in col 1 ; octane
+octane = gas[, 1]
+
+nir = as.matrix(gas[, -1]) # removed the octane variable 
+# converting to matrix for it takes less memory space 
+  # and we need the matrix form for later usagee 
+
+phi = cor(nir, octane)/apply(nir, 2, sd)
+# 2 because we want to use in all columns, if row use 1 
+
+v = nir %*% phi # matrix multiplication 
+
+# run final regression
+fwd = glm(octane ~ v)
+summary(fwd)
+
+# further code in github link 
+  # office hours 4-5pm 
+
+
+# next class probably flash coding , be prepared 
+
+
+
+getwd()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
