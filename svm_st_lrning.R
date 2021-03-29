438d69151408e6215f7808e993237fbbe217f25b
tcomr <tcomraaee@gmail.com>
2021-03-29 00:40:29 -0400

    predict

--- svm_st_lrning.R
+++ svm_st_lrning.R
@@ -30,18 +30,51 @@ plot(svmfit, dat)
 svmfit$index
 
 
+# tune for ten-fold (default) cross validation 
+
+
+tune.out = tune(svm, y~., data = dat, kernel = 'linear',
+                ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
 
 
 
+summary(tune.out)
+# Parameter tuning of 'svm':
+#   
+#   - sampling method: 10-fold cross validation 
+# 
+# - best parameters:
+#   cost
+# 5
+# 
+# - best performance: 0.1 
+# 
+# - Detailed performance results:
+#   cost error dispersion
+# 1 1e-03  0.65  0.4743416
+# 2 1e-02  0.65  0.4743416
+# 3 1e-01  0.15  0.3374743
+# 4 1e+00  0.15  0.2415229
+# 5 5e+00  0.10  0.2108185
+# 6 1e+01  0.10  0.2108185
+# 7 1e+02  0.15  0.2415229
 
+# best.model 
 
+bestmod = tune.out$best.model
+summary(bestmod)
 
+# create a test data 
+xtest = matrix(rnorm(20*2), ncol = 2)
+ytest = sample(c(-1, 1), 20, rep = TRUE)
+ytest
 
+xtest[ytest == 1, ] = xtest[ytest ==1, ] + 1 
+testdat = data.frame(x = xtest, y = as.factor(ytest))
 
+# predict
+ypred = predict(bestmod, testdat)
+ypred
+summary(ypred)
 
-
-
-
-
-
-
+table(predict = ypred, truth = testdat$y)
