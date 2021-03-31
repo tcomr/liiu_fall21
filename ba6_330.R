2429334fda816baece8a0db15a1c5fc436702ba6
tcomr <tcomraaee@gmail.com>
2021-03-30 22:05:20 -0400

    partial least square textir congress109

---
+++ ba6_330.R
@@ -0,0 +1,15 @@
+# Partial least square 
+
+rm(list = ls())
+
+install.packages("textir")
+  # it has built congress dataset 
+  # what they said, and how they voted in 2004
+
+library(textir)
+
+data(congress109)
+
+# count dataset: partial least square 
+
+# congress109ideology dataset 
