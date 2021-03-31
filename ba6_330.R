116d3e95a2a38c372e741e1f1b6b4e9c6e6739f8
tcomr <tcomraaee@gmail.com>
2021-03-30 22:41:12 -0400

    pls loadings direction

--- ba6_330.R
+++ ba6_330.R
@@ -11,5 +11,55 @@ library(textir)
 data(congress109)
 
 # count dataset: partial least square 
+  # process the data in dtm 
+  # everyword in a column 
 
-# congress109ideology dataset 
+# congress109ideology dataset: 529 
+
+congress109Counts[c("Chris Cannon", "Michael Conaway"), 1:5]
+
+congress109Counts[c("Chris Cannon", "Michael Conaway"), 935:940]
+
+
+# we will PLS; we relate with repshare variaable
+# can we predict the political affiliation to predict party
+# create a slant index to predict party afffiliation 
+
+# Create an index to find this person to that purchase 
+
+# data provided by Matt Tady 
+
+str(congress109Counts)
+
+# can you predict his/her party affiliation 
+  # from what he/she said 
+# Average political ideology from their speech 
+
+# create variable 
+f = congress109Counts
+y = congress109Ideology$repshare
+
+# Partial Least Square / boosting 
+  # mattady github repo
+
+
+slant = pls(f, y, K = 3)
+# k is how many iteration 
+  # there is ML wasy of figuring out k 
+
+summary(slant)
+
+slant$loadings
+# gives different coefficients of a word
+  # three different coeffs. 
+slant$directions
+
+
+
+
+
+
+
+
+
+
