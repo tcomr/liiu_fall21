51c6782ca9b9c057fd032489d2e6b5c2a515c119
tcomr <tcomraaee@gmail.com>
2021-04-01 21:19:01 -0400

    ses exponentil smoothing forecast

--- ts6.R
+++ ts6.R
@@ -16,3 +16,34 @@ autoplot(fc) +
 # blue line: point forecast
 # dark shared area: 80% prediction interval 
   # 80% probability that the area will include the prediction 
+
+# smaller range: less certain, more uncertain 
+# wider range: high certain, less uncertain 
+# The range gets wider as we move along the time 
+
+# The closer the forecasting horizon is, the better teh forecasting is 
+
+
+summary(fc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
